dd9ad62f5bb9f0abe82cfcdbfe4ad2557a79f5ae
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-02-21 23:12:01 +0330

    Navbar edits

--- src/components/Utility/Navbar/Lang.js
+++ src/components/Utility/Navbar/Lang.js
@@ -2,15 +2,12 @@ import React from 'react';
 
 const Lang = () => {
   return (
-    <select className=' px-3 bg-transparent outline-none cursor-pointer'>
+    <select className=' mx-3 bg-transparent outline-none cursor-pointer'>
       <option value='EN' className='text-black'>
         EN
       </option>
       <option value='FA' className='text-black'>
         FA
-      </option>
-      <option value='AR' className='text-black'>
-        AR
       </option>
     </select>
   );
--- src/components/Utility/Navbar/Navbar.js
+++ src/components/Utility/Navbar/Navbar.js
@@ -1,35 +1,40 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import classes from './Navbar.module.css';
 import Currency from '../Currency';
 import Lang from './Lang';
 
 const Navbar = ({ homepage, cryptoPage }) => {
   return (
-    <div className={`${cryptoPage ? 'bg-gray-800' : 'bg-transparent'} py-5`}>
+    <nav
+      className={`${
+        cryptoPage ? 'bg-gray-800' : 'bg-transparent'
+      } py-5 border-b border-yellow-500`}>
       <div className='container mx-auto text-white flex justify-between items-center'>
-        <div className='font-bold text-3xl'>CryptoNow</div>
+        <Link to='/' className='font-bold text-3xl'>
+          CryptoNow
+        </Link>
         <div>
           <NavLink
             exact
             activeClassName={classes.MyActive}
             to='/'
-            className={`px-3 inline-block ${classes.HoverEffect}`}>
+            className={`mx-3 inline-block ${classes.HoverEffect}`}>
             Home
           </NavLink>
           <NavLink
             to='/converter'
             activeClassName={classes.MyActive}
-            className={`px-3 inline-block ${classes.HoverEffect}`}>
+            className={`mx-3 inline-block ${classes.HoverEffect}`}>
             Converter
           </NavLink>
           {homepage && (
-            <Currency selectClasses='px-3 bg-transparent outline-none cursor-pointer' />
+            <Currency selectClasses='mx-3 bg-transparent outline-none cursor-pointer' />
           )}
           <Lang />
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
