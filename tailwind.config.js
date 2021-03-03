fd2716157fa66da19a9affa85fa4f7145e6ad3ec
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-03-03 15:46:48 +0330

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-    './src/components/Utility/Logo.js',
-  ],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      safelist: ['w-20', 'h-20', 'w-12', 'h-12', 'max-w-full', 'max-h-full'],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
